fdc1c93a572c78a44eb56736088935b34b79f25f
Saahil Parikh <saahilparikh2000@gmail.com>
2020-11-16 20:34:39 -0800

    moved the quadcopter math stuff to the repo

---
+++ src/apps/quadcopter/quad_math.h
@@ -0,0 +1,30 @@
+typedef struct{
+    float M1;
+    float M2;
+    float M3;
+    float M4;
+} indiv_moments;
+
+typedef struct{
+    float F1;
+    float F2;
+    float F3;
+    float F4;
+} indiv_forces;
+
+typedef struct{
+    float m_z;
+    float m_x;
+    float m_y;
+} moment;
+
+typedef struct{
+    float m_z;
+    float m_x;
+    float m_y;
+} moment;
+
+float get_RPM(int voltage, int duty_cycle);
+float get_force(int angular_velocity);
+float get_moment(int angular_velocity);
+moment get_moments(moment* moment, indiv_moments *ind_moment, indiv_forces *ind_forces);
---
+++ src/apps/quadcopter/quadcopter_constants.h
@@ -0,0 +1,5 @@
+/** determine later**/
+#define k_F 100
+#define k_M 100
+#define KV 2200
+#define to_center_length 5
